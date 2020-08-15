601ae0f279104cc46f2c560879bd5303c5952bbe
vovazavislyak <42644968+vovazavislyak@users.noreply.github.com>
2020-08-15 21:39:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(function(){
-    $('.header__slider-inner, .hot__deals-inner').slick(
-        {
+    $('.header__slider-inner, .hot__deals-inner').slick({
         arrows: false,
         dots: true
-    }
-    );
+    });
 
 
     $('.tab__control .item').on('click', function(){
-        // alert('work');
         if (this.classList.contains('active__item'))
             return;
         var active = document.querySelector('.tab__control .active__item');
         active.classList.remove('active__item');
         this.classList.add("active__item");
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu ul').slideToggle();
     });
 });
