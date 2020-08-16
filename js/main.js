85fadefcfa3abc4d34bd12a1bf3932e16464e428
vovazavislyak <42644968+vovazavislyak@users.noreply.github.com>
2020-08-16 22:44:58 +0300

    Add 3 new page

--- js/main.js
+++ js/main.js
@@ -4,16 +4,32 @@ $(function(){
         dots: true
     });
 
+    $('.product__slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.product__slider-nav'
+      });
+      $('.product__slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.product__slider-main',
+        // dots: true,
+        // centerMode: true,
+        focusOnSelect: true
+      });
 
-    $('.tab__control .item').on('click', function(){
+    $('.tab__control .item, .products__category li').on('click', function(){
         if (this.classList.contains('active__item'))
             return;
-        var active = document.querySelector('.tab__control .active__item');
+        var active = document.querySelector('.tab__control .active__item, ' +
+                                            '.products__category .active__item');
         active.classList.remove('active__item');
         this.classList.add("active__item");
     });
 
     $('.menu__btn').on('click', function(){
-        $('.menu ul').slideToggle();
+        $('.header .menu ul').slideToggle();
     });
 });
